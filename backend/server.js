01e8e09738fc8deb0ca5fadb287e274e528aa441
AbigailMedina <AMEDINA2016@columbiasecondary.org>
2019-03-12 18:48:17 -0400

    still heroku testing

--- backend/server.js
+++ backend/server.js
@@ -107,7 +107,7 @@ router.route('/:userId/projects').get(function (req, res){
 app.use('/api', router);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static( '../client/build' ));
+    app.use(express.static(path.join(__dirname, '../client/build')));
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
