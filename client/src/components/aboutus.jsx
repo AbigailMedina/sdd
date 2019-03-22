9b05c237e7a9bd6658e680779850962354463c5b
Pragati Pant <pragatipant@esker-29.dynamic.rpi.edu>
2019-03-22 16:25:00 -0400

    added eric photo

--- client/src/components/aboutus.jsx
+++ client/src/components/aboutus.jsx
@@ -32,60 +32,69 @@ class AboutUs extends Component {
               						<img src={require("../photos/pragati.JPG")} alt="pragati"/>
             						</figure>
           						</div>
-          					<br></br>
+          					<br/>
           					<div class="center">
           						<h4>Pragati Pant</h4>
-          						<p>Hi! I am a junior CS major at RPI. I interned at Fidelity Investments last summer, and will be interning at Morgan Stanley this summer. In my free time, I enjoy watching Netflix and baking.</p>
+          						<p>Hi! I am a junior CS major at RPI. I interned at Fidelity Investments last summer, and 
+          							will be interning at Morgan Stanley this summer. In my free time, I enjoy watching Netflix and baking.</p>
           					</div>
         				</div>
       				</div>
       				<div class="column">
         				<div class="card">
-          					<div class="card-image">
-            					<figure class="image is-4by3">
-              						<img src={require("../photos/abby.jpg")} alt="abby"></img>
-            					</figure>
-            				</div>
-            				<br></br>
-          					<div class="center">
-          						<h4>Abigail Medina</h4>
-          						<p>Hello! I am a junior Computer Science major at RPI. I interned at Aarazoo last summer, and will be interning at Prudential Financial this summer. I spend my free time cooking and watching Netflix.</p>
-        						</div>
+          				<div class="card-image">
+            				<figure class="image is-4by3">
+             					<img src={require("../photos/abby.jpg")} alt="abby"></img>
+           					</figure>
+           				</div>
+           				<br/>
+         					<div class="center">
+         						<h4>Abigail Medina</h4>
+         						<p>Hello! I am a junior Computer Science major at RPI. I interned at Aarazoo last summer, and will be 
+         							interning at Prudential Financial this summer. I spend my free time cooking and watching Netflix.</p>
+       						</div>
       					</div>
       				</div>
       				<div class="column">
         				<div class="card">
-          					<div class="card-image">
-            					<figure class="image is-4by3">
-              						<img src="https://source.unsplash.com/random/1280x960" alt="Placeholder image"></img>
-            					</figure>
-          					</div>
-          					<h4>Eric Lawson</h4>
-          					<p>Lorem ipsum fuck this shit.</p>
+          				<div class="card-image">
+            				<figure class="image is-4by3">
+              					<img src="https://source.unsplash.com/random/1280x960" alt="Placeholder image"></img>
+            				</figure>
+          				</div>
+         					<br/>
+         					<div class="center">
+         						<h4>Eric Lawson</h4>
+         						<p>Lorem ipsum fuck this shit.</p>
+         					</div>
         				</div>
       				</div>
       				<div class="column">
         				<div class="card">
-          					<div class="card-image">
-            					<figure class="image is-4by3">
-              						<img src="https://source.unsplash.com/random/1280x960" alt="Placeholder image"></img>
-            					</figure>
-          					</div>
+          				<div class="card-image">
+            				<figure class="image is-4by3">
+              					<img src="https://source.unsplash.com/random/1280x960" alt="Placeholder image"></img>
+            				</figure>
+          				</div>
+          				<br/>
+          				<div class="center">
           					<h4>Kiki Sanusi</h4>
           					<p>Hi! I'm Eric!</p>
+          				</div>
         				</div>
       				</div>
       				<div class="column">
         				<div class="card">
           					<div class="card-image">
             					<figure class="image is-4by3">
-              						<img src="https://source.unsplash.com/random/1280x960" alt="Placeholder image"></img>
+              						<img src={require("../photos/ericx.jpg")} alt="eric"></img>
             					</figure>
           					</div>
           					<br/>
           					<div class="center">
           						<h4>Eric Xu</h4>
-          						<p>Hi! I'm Eric! As a third year computer science student at RPI, my experience comes from projects for my classes. Working on ShutUp! has been very exciting. I have enjoyed working with other students.</p>
+          						<p>Hi! I'm Eric! As a third year computer science student at RPI, my experience comes from projects 
+          							for my classes. Working on ShutUp! has been very exciting. I have enjoyed working with other students.</p>
         						</div>
         				</div>
       				</div>
