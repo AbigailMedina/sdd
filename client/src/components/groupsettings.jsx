717acc2fbb41f03616487e882757ff098070d58c
AbigailMedina <AMEDINA2016@columbiasecondary.org>
2019-03-23 16:54:41 -0400

    remove functionality written, needs styling

--- client/src/components/groupsettings.jsx
+++ client/src/components/groupsettings.jsx
@@ -34,64 +34,67 @@ class GroupSettings extends Component {
     }
 
     showCollaborators(){
-    	console.log("collaborators1",this.state.collaborators)
-    	var content = this.state.collaborators.slice()
-    	if(content.length == 0 ){
-    		console.log("no collaborators")
-    		return(
-				<div className="field is-grouped">
-				 	<div className="control">
-				    	<input 
-					    	className={this.state.userError?"input is-danger":"input is-info"} 
-					    	value = {this.state.userError? "User does not exist":this.state.email} 
-					    	onChange = {this.onChangeEmail.bind(this)} type="email" placeholder="Email input" />
-					</div>
-				  	<div className="control">
-					    <button 
-					    	className="button is-link" 
-					    	disabled={!this.state.email} 
-					    	onClick={this.onAddCollaborator.bind(this)}>Add another</button>
-					</div>
+    	// console.log("collaborators1",this.state.collaborators)    	
+    	var content = this.state.collaborators.map((collaborator) => {
+		return( 
+			<li className = "level" key={collaborator}>{collaborator}
+				<div className="control">
+				    <button className="button is-danger" onClick={() =>{
+				    	console.log("remove", collaborator)
+				    	this.onRemoveCollaborator(collaborator)
+				    }}>Remove collaborator</button>
 				</div>
-    		)
-    	}else{
-    		
-			content = this.state.collaborators.map((collaborator) => {
-				return(
-				<li className = "level" key={collaborator}>{collaborator}
-					<div className="control">
-					    <button className="button is-danger" onClick={() =>
-					    	console.log("remove", collaborator)
-					    	//will send patch here to /projects/:id/removeUser/:userID
-					    }>Remove collaborator</button>
-					</div>
-				</li> 
+			</li> 
 			)
-			})
-			console.log("collaborators:",content)
-			return content;
-			
-    	}		
+		})
+		return content;
     }
     onChangeEmail(e){
 		this.setState({email:e.target.value,userError:false})
+	}
+	updateProject(newArray){
+		const { match: { params } } = this.props;
+
+		axios.patch(`http://localhost:5000/projects/${params.id}`,{collaborators:newArray}).then(response => {
+            console.log("updated project: ",response.data.project)
+            this.setState({
+            	projectName:response.data.project.name,
+            	collaborators:response.data.project.collaborators})
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
 	}
 	onAddCollaborator(){
 		//verify that email == a real user
 		//using uri2 for local development
 		axios.get(`http://localhost:5000/users/${this.state.email}`).then(response => {
-            console.log("user found in createProject: ",response.data.user)
+            console.log("user found in groupSettings: ",response.data.user)
            	var newArray = this.state.collaborators.slice();    
 	    	newArray.push(this.state.email);  
 			this.setState({collaborators:newArray},
 				()=>{
-					this.setState({email:""})
+					this.setState({email:""});
+					this.updateProject(newArray)
 				})
         })
         .catch( error =>{
             console.log(error);
             this.setState({userError:true})
         })
+        
+	}
+	onRemoveCollaborator(removeMe){
+		const newArray = this.state.collaborators.filter(
+			function (collaborator) {
+		  		return collaborator !== removeMe;
+			});
+		
+	    this.setState({collaborators:newArray});
+		console.log(this.state.collaborators);
+
+		this.updateProject(newArray)
+		
 	}
     
 	render() {
@@ -101,8 +104,23 @@ class GroupSettings extends Component {
 	    		<Sidebar className="column is-one-quarter"/>
 	    		<div className="column is-three-quarters" style={{marginTop:"100px"}}>
 	    			<h2 class="title is-2">Group Settings for {this.state.projectName}</h2>
-					<label className="label" onClick={this.showCollaborators.bind(this)}>Collaborator Emails</label>
-					<span>{collaborators()}</span>
+					<label className="label">Collaborator Emails</label>
+					
+					<span>{collaborators()}</span>					
+					<div className="field is-grouped">
+					 	<div className="control">
+					    	<input 
+						    	className={this.state.userError?"input is-danger":"input is-info"} 
+						    	value = {this.state.userError? "User does not exist":this.state.email} 
+						    	onChange = {this.onChangeEmail.bind(this)} type="email" placeholder="Email input" />
+						</div>
+					  	<div className="control">
+						    <button 
+						    	className="button is-link" 
+						    	disabled={!this.state.email} 
+						    	onClick={this.onAddCollaborator.bind(this)}>Add another</button>
+						</div>
+					</div>
 				</div>				
 	    	</div>
 	    )
