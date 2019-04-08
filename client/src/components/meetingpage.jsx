1ba96069b303b17b7bb139ab461337c7b90f7640
Pragati Pant <pragatipant@heffner-wl-85.dynamic.rpi.edu>
2019-04-08 12:53:12 -0400

    formatting on meetinpage display fixed

--- client/src/components/meetingpage.jsx
+++ client/src/components/meetingpage.jsx
@@ -7,38 +7,41 @@ import 'bulma/css/bulma.css'
 import ButtonComponent from './buttonComponent.jsx';
 
 class MeetingPage extends Component {
+	
 	constructor(props) {
 		const uri = "https://sdd-shutup.herokuapp.com"
   		const uri2 = "http://localhost:5000"
   		super(props);
 	    
 	}
+
 	componentDidMount(props) {
   		const { match: { params } } = this.props;
   		//using uri2
   		axios.get(`http://localhost:5000/projects/${params.id}`).then(response => {
-                console.log("project found in settings: ",response.data.project)
-                this.setState({project:response.data.project.name})
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
+            console.log("project found in settings: ",response.data.project)
+            this.setState({project:response.data.project.name})
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
     }
 
   	render() {
 	    return (
-	    	<div class="columns">
-	    		<Sidebar class="column is-one-quarter"/>
-	    		<div class="columns column is-three-quarters level" style = {{marginTop:"100px"}}>
-	    			
-	    			<ButtonComponent class="column is-one-quarter level"/>
-	    			
-						<TimerComponent class="column is-one-quarter level"/>	
-				
+	    	<div class="columns" >
+	    		<div class="column is-one-quarter level">
+	    			<Sidebar />
 	    		</div>
+	    		<div class="column is-one-quarter level" style = {{marginTop:"100px"}}>
+	    			<ButtonComponent />
+	    		</div>
+	    		<div class="column is-one-quarter level" style = {{marginTop:"100px"}}>
+					<TimerComponent	/>
+				</div>
 	    	</div>
 	    )
-  }
+  	}
 }
 
 export default MeetingPage;
