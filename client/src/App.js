c1289836a4048c82b3ea3a660f4149663b7131f5
ksanusi <ksanusif@gmail.com>
2019-04-12 20:07:32 -0400

    Revert "app.js"
    This reverts commit b340fd959801bc34a90e1bf93f7ff454ea80d306.

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { Component } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import Cookies from 'universal-cookie';
@@ -144,3 +145,108 @@ class App extends Component {
 }
 
 export default App;
+=======
+import React, { Component } from 'react';
+import { HashRouter, Route } from 'react-router-dom';
+import './App.css';
+import HomePage from './components/homepage';
+import LoginPage from './components/loginpage';
+import SignUpPage from './components/signupPage';
+import AboutUs from './components/aboutus';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
+import CreateProject from './components/createProject';
+import MeetingPage from './components/meetingpage';
+import UserSettings from './components/usersettings';
+import GroupSettings from './components/groupsettings';
+import Chat from './components/chat';
+import OldNotesPage from './components/OldNotesPage';
+
+// main class used to establish routes to component pages
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      userId: null,
+      loggedIn: false
+    }
+  }
+
+  onLogin(user) {
+    console.log(user._id);
+    this.setState({
+      userId: user._id,
+      loggedIn: true
+    })
+  }
+
+  onLogout() {
+    this.setState({
+      userId: null,
+      loggedIn: false
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <HashRouter>
+        <div>
+          <Navbar
+            userId={this.state.userId}
+          />
+          <Route 
+            exact path="/" 
+            component={HomePage} 
+          />
+          <Route 
+            exact path="/login" 
+            render={() => <LoginPage 
+              login={this.onLogin.bind(this)}
+              logout={this.onLogout.bind(this)}
+            />}
+          />
+          <Route 
+            exact path="/signUp" 
+            component={SignUpPage} 
+          />
+          <Route 
+            exact path="/AboutUs" 
+            component={AboutUs} 
+          />
+          <Route 
+            exact path="/add" 
+            component={CreateProject} 
+          />
+          <Route
+            exact path="/meeting/:id"
+            component={MeetingPage}
+          />
+          <Route
+            exact path="/chat/:id"
+            component={Chat}
+          />
+          <Route
+            path="/groupsettings/:id"
+            component={GroupSettings}
+          />
+          <Route
+            exact path="/settings/:id"
+            component={UserSettings}
+          />
+          <Route
+            exact path="/oldnotes/:id"
+            component={OldNotesPage}
+          />           
+         <Footer/>
+        </div>
+      </HashRouter>
+
+      </div>
+    );
+  }
+}
+
+export default App;
+>>>>>>> parent of a92acb7... Merge branch 'master' into userSpecific
