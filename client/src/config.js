3406512f8610333495a805795e5fe1a2e8946cb6
Eric Lawson <elawson668@gmail.com>
2019-04-24 20:24:18 -0400

    config files, remove node_modules

---
+++ client/src/config.js
@@ -0,0 +1,5 @@
+const instanceLocator = 'v1:us1:1ac58f9f-8dfe-4f8f-bdcf-95ea233fe7f6'
+const secretKey = 'ff9ae820-8f89-4a87-820c-131d94b4fe7a:gZnsTHSgDMZ8s6tZGqEgILemkDImFRk+aSuliOCRjeU='
+const tokenUrl = 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/1ac58f9f-8dfe-4f8f-bdcf-95ea233fe7f6/token'
+
+export {instanceLocator, secretKey, tokenUrl}
